834cb342f98e203c7555522060089a5f9a112ddc
James Powis <j.powis@gmail.com>
2025-03-31 22:05:51 +1100

    initial

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 'use strict';
 
+//imports the Express module
 const express = require('express');
 
+//sets variables for host and port
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+//creates an instance of the Express application
 const app = express();
+
+//define a route for the root url that responds with a message
 app.get('/', (req, res) => {
-    res.send('Hello World.  This is week 2 practical repurposed for week 5!');
+    res.send('Hello World.  This is week 2 practical repurposed for week 5!'); //sets the message
 });
 
+//start the server and listen on the port and host specified
 app.listen(PORT, HOST, () => {
-    console.log('Running on http://' + HOST + ':' + PORT);
+    console.log('Running on http://' + HOST + ':' + PORT); //logs to console that the server is running and on what host and port
 });
